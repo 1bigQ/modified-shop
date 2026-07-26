62c7c057e0e23af26b4e17e962112f4def2dbd26
GTB <gerhard@modified-shop.org>
2026-07-26 15:32:48 +0200

    update iso codes for some countries (#3196)
    * update iso codes for some countries
    
    * update iso codes in used shipping modules
    
    * fix country name for first install
    
    ---------
    
    Co-authored-by: Tomcraft1980 <Tomcraft1980@users.noreply.github.com>

--- _installer/update/update_3.3.0_to_3.3.1.sql
+++ _installer/update/update_3.3.0_to_3.3.1.sql
@@ -13,6 +13,15 @@ INSERT INTO `database_version` (`version`, `date_added`) VALUES ('MOD_3.3.1', NO
 #GTB - 2026-07-20 - set index to speed up admin dashboard turnover query
 ALTER TABLE `orders_total` ADD INDEX `idx_class` (`class`, `orders_id`);
 
+#GTB - 2026-07-23 - update iso codes for some countries
+UPDATE `countries` SET `countries_iso_code_3` = 'AUS' WHERE countries_iso_code_2 = 'AU' AND countries_iso_code_3 = 'AUD';
+UPDATE `countries` SET `countries_iso_code_3` = 'ROU' WHERE countries_iso_code_2 = 'RO' AND countries_iso_code_3 = 'ROM';
+UPDATE `countries` SET `countries_name` = 'Timor-Leste', `countries_iso_code_2` = 'TL', `countries_iso_code_3` = 'TLS' WHERE countries_iso_code_2 = 'TP' AND countries_iso_code_3 = 'TMP';
+
+#GTB - 2026-07-23 - migrate TP to TL in saved shipping module country lists (East Timor iso code change above)
+UPDATE `configuration` SET `configuration_value` = TRIM(BOTH ',' FROM REPLACE(CONCAT(',', `configuration_value`, ','), ',TP,', ',TL,')) WHERE `configuration_key` = 'MODULE_SHIPPING_DHL_COUNTRIES_10';
+UPDATE `configuration` SET `configuration_value` = TRIM(BOTH ',' FROM REPLACE(CONCAT(',', `configuration_value`, ','), ',TP,', ',TL,')) WHERE `configuration_key` = 'MODULE_SHIPPING_CHP_COUNTRIES_7';
+UPDATE `configuration` SET `configuration_value` = TRIM(BOTH ',' FROM REPLACE(CONCAT(',', `configuration_value`, ','), ',TP,', ',TL,')) WHERE `configuration_key` = 'MODULE_SHIPPING_AP_COUNTRIES_5';
 #GTB - 2026-07-24 - update DPD tracking link
 UPDATE `carriers` SET `carrier_tracking_link` = 'https://my.dpd.de/redirect.aspx?action=2&parcelno=$1&locale=$2' WHERE `carrier_tracking_link` = 'https://extranet.dpd.de/cgi-bin/delistrack?pknr=$1+&typ=1&lang=$2';
 
